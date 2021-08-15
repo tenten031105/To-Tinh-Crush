3417f644b77a3eef0e315e09cebb46057ee8456b
tenten031105 <86817434+tenten031105@users.noreply.github.com>
2021-08-15 15:30:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'hiluuuuuu tphuuu<3',
+    introDesc: `người dthuongggg nhứt quả đất này là aii??,
+    btnIntro: 'Thủy Tiên chứ ai=))',
+    title: 'z bạn mê Thủy Tiên lắm chứ gì',
+    desc: 'nói thiệt i đừng ngại nhe ',
+    btnYes: 'hehe đúng gòi<33',
+    btnNo: 'Khum :3',
+    question:'z sao bạn mê Thủy Tiên z',
+    btnReply: 'lý do....',
+    reply: 'tại tten xing đẹp tỵt zờiiiiii <33333333',
+    mess: 'heheh bít ngay mè',
+    messDesc: 'z tphu có tặng cho tten ly trà đào honggg?',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
